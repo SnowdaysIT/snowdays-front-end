5c0c9ccfacea50e88ede7c56367ebc645a65b1dd
Marco Mondini || Knopfy <mmondini@mondspace.com>
2019-12-22 22:29:33 +0200

    ✨ apollo-client dependencies, client draft

---
+++ src/components/api/Client.js
@@ -0,0 +1,7 @@
+import ApolloClient from 'apollo-boost';
+
+export default new ApolloClient({
+  // TODO backend api endpoint => retrieve from
+  // (e.g.) env variables or global configs
+  uri: 'https://48p1r2roz4.sse.codesandbox.io',
+});
